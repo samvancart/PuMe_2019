2cd9e31028a6124f5b9f213b41a1399b20a46e2f
samvancart <sam02cart@gmail.com>
2021-02-01 08:09:44 +0200

    modified Rprebas library files

---
+++ R-Portable/App/R-Portable/library/Rprebas/R/prebas.r
@@ -0,0 +1,224 @@
+
+prebas <- function(nYears,
+                   pCROBAS = pCROB,
+                   pHcMod = pHcM,
+                   pPRELES = pPREL,
+                   pYASSO = pYAS,
+                   pAWEN = parsAWEN,
+                   # PREBASversion = 0,
+                   etmodel = 0,
+                   siteInfo = NA,
+                   thinning=NA,
+                   initClearcut = c(1.5,0.5,0.0431969,0.,0.),
+                   fixBAinitClarcut = 1.,
+                   initCLcutRatio = NA,
+                   PAR,TAir,VPD,Precip,CO2,
+                   P0=NA,
+                   initVar = NA,
+                   soilC = NA,
+                   weatherYasso = NA,
+                   litterSize = litterSizeDef,
+                   soilCtot = NA,
+                   defaultThin = 1.,
+                   ClCut = 1.,
+                   energyCut = 0.,
+                   inDclct = NA,
+                   inAclct = NA,
+                   yassoRun = 0,
+                   smoothP0 = 1,
+                   smoothETS = 1,
+                   smoothYear=5,
+                   tapioPars=pTapio,
+                   thdPer=0.5,
+                   limPer=0.5,
+                   ftTapioPar = ftTapio,
+                   tTapioPar = tTapio){
+  
+  ###process weather###
+  if(length(PAR) >= (nYears*365)){
+    PAR = PAR[1:(nYears*365)]
+    TAir = TAir[1:(nYears*365)]
+    VPD = VPD[1:(nYears*365)]
+    Precip = Precip[1:(nYears*365)]
+    CO2 = CO2[1:(nYears*365)]
+  } else{
+    stop("daily weather inputs < nYears*365")
+  }
+  ###
+  
+  ###proc thinnings##
+  if(all(is.na(thinning))){
+    thinning=matrix(0,1,9)
+    thinning[,9] <- -999
+  } 
+  thinning[is.na(thinning)] <- -999
+  nThinning = max(1,nrow(thinning))
+  thinning <- thinning[order(thinning[,2],thinning[,1],thinning[,3]),]
+  ###
+  if(all(is.na(initVar))) {
+    nLayers <- 3 }else {
+      nLayers <- ifelse(is.null(ncol(initVar)),1,ncol(initVar))
+    }
+  nSp = ncol(pCROBAS)
+  if(anyNA(siteInfo)) siteInfo = c(1,1,3,160,0,0,20,413.,0.45,0.118) ###default values for nspecies and site type = 3
+                                  
+  if(all(is.na(initCLcutRatio))){
+    initCLcutRatio <- rep(1/nLayers,nLayers)
+  }
+  
+  varNam <- getVarNam()
+  nVar <- length(varNam)
+  
+  layerNam <- paste("layer",1:nLayers)
+  output <- array(0, dim=c((nYears),nVar,nLayers,2),
+                  dimnames = list(year=NULL,variable=varNam,layer=layerNam,status=c("stand","thinned")))
+  energyWood <- array(0, dim=c((nYears),nLayers,2),
+                      dimnames = list(year=NULL,layer=layerNam,variable=c("volume","biomass")))
+  fAPAR <- rep(0.7,nYears)
+  
+  ###compute ETS year
+  Temp <- TAir[1:(365*nYears)]-5
+  ETS <- pmax(0,Temp,na.rm=T)
+  ETS <- matrix(ETS,365,nYears); ETS <- colSums(ETS)
+  if(smoothETS==1.) for(i in 2:nYears) ETS[i] <- ETS[(i-1)] + (ETS[i]-ETS[(i-1)])/min(i,smoothYear)
+  
+  ###if P0 is not provided use preles to compute P0
+  if(is.na(P0)){
+    P0 <- PRELES(DOY=rep(1:365,nYears),
+                 PAR=PAR,TAir=TAir,VPD=VPD,Precip=Precip,CO2=CO2,
+                 fAPAR=rep(1,length(PAR)),LOGFLAG=0,p=pPRELES)$GPP
+    P0 <- matrix(P0,365,nYears);P0 <- colSums(P0)
+  }
+  P0 <- matrix(P0,nYears,2)
+  if(smoothP0==1.){
+    P0[1,2] <- P0[1,1]
+    for(i in 2:nYears) P0[i,2] <- P0[(i-1),2] + (P0[i,1]-P0[(i-1),2])/min(i,smoothYear)
+  } 
+  
+  ETSthres <- 1000; ETSmean <- mean(ETS)
+  
+  ####process clearcut
+  if(any(!is.na(c(inDclct,inAclct)))){
+    if(is.na(inDclct)) inDclct <- 9999999.99
+    if(is.na(inAclct)) inAclct <- 9999999.99
+  }
+  # if(ClCut==1 & all(is.na(initVar)) & is.na(inDclct)) inDclct <-
+  if(ClCut==1 & all(is.na(inDclct))) inDclct <-
+    c(ClCutD_Pine(ETSmean,ETSthres,siteInfo[3]), ####pine in Finland
+      ClCutD_Spruce(ETSmean,ETSthres,siteInfo[3]), ####spruce in Finland
+      ClCutD_Birch(ETSmean,ETSthres,siteInfo[3]), ####birch in Finland
+      NA,NA,NA,NA)  ###"fasy","pipi","eugl","rops"
+  # if(ClCut==1 & all(is.na(initVar)) & is.na(inAclct)) inAclct <-
+  if(ClCut==1 & all(is.na(inAclct))) inAclct <-
+    c(ClCutA_Pine(ETSmean,ETSthres,siteInfo[3]),   ####pine in Finland
+      ClCutA_Spruce(ETSmean,ETSthres,siteInfo[3]), ####spruce in Finland 
+      ClCutA_Birch(ETSmean,ETSthres,siteInfo[3]),  ####birch in Finland
+      80,50,13,30)  ###"fasy","pipi","eugl","rops"  
+  if(any(is.na(inDclct))) inDclct[is.na(inDclct)] <- 9999999.99
+  if(length(inDclct)==1) inDclct<- rep(inDclct,nSp)
+  if(any(is.na(inAclct))) inAclct[is.na(inAclct)] <- 9999999.99
+  if(length(inAclct)==1) inAclct<- rep(inAclct,nSp)
+  
+  ###if any initial value is given the model is initialized from plantation
+  if (all(is.na(initVar))){
+    initVar <- matrix(NA,7,nLayers)
+    initVar[1,] <- 1:nLayers
+    initVar[3,] <- initClearcut[1]; initVar[4,] <- initClearcut[2]
+    initVar[5,] <- initClearcut[3]/nLayers; initVar[6,] <- initClearcut[4]
+  }
+  
+  
+  ####if Height of the crown base is not available use model
+  initVar <- findHcNAs(initVar,pHcMod)
+  # initialize A
+  for(ikj in 1:nLayers){
+    p_ksi=pCROBAS[38,initVar[1,ikj]]
+    p_rhof <- pCROBAS[15,initVar[1,ikj]]
+    p_z <- pCROBAS[11,initVar[1,ikj]]
+    Lc <- initVar[3,ikj] - initVar[6,ikj]
+    A <- p_ksi/p_rhof * Lc^p_z
+    initVar[7,ikj] <- A     
+  } 
+  
+  # print(initVar)
+  xx <- min(10,nYears)
+  Ainit = 6 + 2*siteInfo[3] - 0.005*(sum(ETS[1:xx])/xx) + 2.25
+  if(length(initVar[2,which(is.na(initVar[2,]))])>0){
+     initVar[2,which(is.na(initVar[2,]))] <- as.numeric(round(Ainit))
+    }
+  # print(initVar)
+  
+  ####process weather PRELES (!!to check 365/366 days per year)
+  weatherPreles <- array(c(PAR,TAir,VPD,Precip,CO2),dim=c(365,nYears,5))
+  weatherPreles <- aperm(weatherPreles, c(2,1,3))
+  
+  ###initialise soil inputs
+  if(all(is.na(soilCtot))) soilCtot = numeric(nYears)
+  if(all(is.na(soilC))) soilC = array(0,dim = c(nYears,5,3,nLayers))
+  # if(all(is.na(litterSize))){
+  #   litterSize = matrix(0,3,nLayers)
+  #   litterSize[2,] <- 2
+  #   for (i in 1:nLayers) litterSize[1,i] <- ifelse(initVar[1,i]==3,10,30)
+  # }
+  
+  ##process weather inputs for YASSO
+  if(all(is.na(weatherYasso))){
+    weatherYasso = matrix(0,nYears,3)
+    weatherYasso[,1] = aTmean(TAir,nYears)
+    weatherYasso[,3] = aTampl(TAir,nYears)
+    weatherYasso[,2] = aPrecip(Precip,nYears)
+  }
+  
+  ###init biomasses
+  initVarX <- rbind(initVar,siteInfo[3])
+  biomasses <- initBiomasses(pCROBAS,initVarX)
+  biomasses[which(is.na(biomasses))] <- 0.
+  output[1,c(33,25,47:49,24,32,50,51,31,30,54),,1] <- biomasses
+  # print(biomasses)
+  initVar <- initVar[1:7,]
+  # PREBASversion <- paste("prebas_v",PREBASversion,sep='')
+
+  prebas <- .Fortran("prebas",
+                     nYears=as.integer(nYears),
+                     nLayers=as.integer(nLayers),
+                     nSp=as.integer(nSp),
+                     siteInfo = as.numeric(siteInfo),
+                     pCROBAS = as.matrix(pCROBAS),
+                     initVar=as.matrix(initVar),
+                     thinning=as.matrix(thinning),
+                     output=as.array(output),
+                     nThinning=as.integer(nThinning),
+                     maxYearSite=as.integer(nYears),
+                     fAPAR=as.numeric(fAPAR),
+                     initClearcut=as.numeric(initClearcut),
+                     fixBAinitClarcut=as.numeric(fixBAinitClarcut),
+                     initCLcutRatio = as.double(initCLcutRatio),
+                     ETS = as.numeric(ETS),
+                     P0 = as.matrix(P0),
+                     weather=as.array(weatherPreles),
+                     DOY= as.integer(1:365),
+                     pPRELES=as.numeric(pPRELES),
+                     etmodel = as.integer(etmodel),
+                     soilC = as.array(soilC),
+                     pYASSO=as.numeric(pYASSO),
+                     pAWEN = as.matrix(pAWEN),
+                     weatherYasso = as.matrix(weatherYasso),
+                     litterSize = as.matrix(litterSize),
+                     soilCtot=as.numeric(soilCtot),
+                     defaultThin=as.double(defaultThin),
+                     ClCut=as.double(ClCut),
+                     energyCut=as.double(energyCut),
+                     inDclct=as.double(inDclct),
+                     inAclct=as.double(inAclct),
+                     dailyPRELES = matrix(-999,(nYears*365),3),
+                     yassoRun=as.double(yassoRun),
+                     energyWood = as.array(energyWood),
+                     tapioPars = as.array(tapioPars),
+                     thdPer = as.double(thdPer),
+                     limPer = as.double(limPer),
+                     ftTapioPar = as.array(ftTapioPar),
+                     tTapioPar = as.array(tTapioPar))
+  class(prebas) <- "prebas"
+  return(prebas)
+}
+
---
+++ R-Portable/App/R-Portable/library/Rprebas/R/utilStaff.r
@@ -0,0 +1,94 @@
+####init Biomass
+initBiomasses <- function(pCro,initVarX){
+  initVarX<-as.matrix(initVarX) #change vector to matrix when maxlayer=1
+  siteType <- initVarX[8,1]
+  ##set parameters
+  par_betab <- pCro[13,initVarX[1,]]
+  par_x <- pCro[19,initVarX[1,]]
+  par_beta0 <- pCro[12,initVarX[1,]]
+  par_betas <- pCro[14,initVarX[1,]]
+  par_mf <- pCro[8,initVarX[1,]]
+  par_mr <- pCro[9,initVarX[1,]]
+  par_mw <- pCro[10,initVarX[1,]]
+  par_alfar <- pCro[20+pmin(siteType,5),initVarX[1,]]
+  par_c <- pCro[7,initVarX[1,]]
+  par_rhof <- pCro[15,initVarX[1,]]
+  par_rhor <- par_alfar * par_rhof
+  par_rhow <- pCro[2,initVarX[1,]]
+  par_S_branchMod <- pCro[27,initVarX[1,]]
+  gammaC <- 0. #initVarX[8,]
+  Tbd <- 10 #####to include in the parameters
+  
+  ###set variables
+  A <- initVarX[7,]
+  ba <- initVarX[5,]; d <- initVarX[4,]
+  N <- ba/(pi*((d/2/100)^2))
+  h = initVarX[3,]; hc <- initVarX[6,]
+  B = ba/N
+  Lc <- h - hc
+  betab =  par_betab * Lc^(par_x-1)
+  beta0 = par_beta0
+  beta1 = (beta0 + betab + par_betas) 
+  beta2 = 1. - betab - par_betas 		
+  betaC = (beta1 + gammaC * beta2) / par_betas
+  wf_STKG <- pmax(0.,par_rhof * A * N)
+  W_froot = pmax(0.,par_rhor * A * N)  ##to check  ##newX
+  W_wsap = pmax(0.,par_rhow * A * N * (beta1 * h + beta2 * hc)) ##newX
+  W_c = pmax(0.,par_rhow * A * N * hc) #sapwood stem below Crown
+  W_s = pmax(0.,par_rhow * A * N * par_betas * Lc) #sapwood stem within crown
+  W_branch =  pmax(0.,par_rhow * A * N * betab * Lc) #branches biomass
+  Wsh = pmax((A+B+sqrt(A*B)) * hc * par_rhow * N/2.9 - W_c,0) #initialize heart wood, only stem considered. W_bole (total biomass below crown)  - Wc
+  #initialize Wdb dead branches biomass
+  Wdb = pmax(0,ifelse(par_S_branchMod == 1.,Tbd * W_branch * ((0.0337+0.000009749*N)*exp(-0.00456*d^2)+0.00723),
+         Tbd * W_branch *((-0.00513+0.000012*N)*exp((0.00000732-0.000000764*N)*d^2)+0.00467)))
+  W_stem = pmax(0.,W_c + W_s + Wsh)
+
+  W_crs = par_rhow * beta0 * A * h * N
+  W_crh = Wsh * beta0
+  W_croot = W_crh + W_crs
+  #W_croot = pmax(0.,(par_rhow * Lc * beta0 * A / par_betas * N + (W_c + Wsh) * beta0)) #coarse root biomass
+  V = W_stem / par_rhow
+  biomasses <- rbind(wf_STKG,W_froot,W_wsap,W_c,W_s,W_branch,W_croot,Wsh,Wdb,W_stem,V,W_crh)
+
+  return(biomasses)
+}
+
+#### function to calculate initial sapwood area at crown base (A)
+compA <- function(inputs){
+  p_ksi = inputs[1]
+  p_rhof = inputs[2]
+  p_z <- inputs[3]
+  Lc = inputs[4]
+  A <- max(0.,p_ksi/p_rhof * Lc^p_z)
+  return(A)
+}
+
+varNames  <- c('siteID','gammaC','sitetype','species','ETS' ,'P0','age', 'DeadWoodVolume', 'Respi_tot','GPP/1000',
+               'H','D', 'BA','Hc_base','Cw','Ac','N','npp','leff','keff','lproj','ET_preles','weight',
+               'Wbranch',"WfineRoots",'Litter_fol','Litter_fr','Litter_branch','Litter_wood','V',
+               'Wstem','W_croot','wf_STKG', 'wf_treeKG','B_tree','Light',"Vharvested","Wharvested","soilC",
+               "aSW","dH","Vmort","gross growth", "GPPspecies","Rh species", "NEP sp"," W_wsap","W_c","W_s","Wsh","Wdb","dHc",
+               "Wbh","Wcrh")
+
+  getVarNam <- function(){
+    return(varNames)
+}
+
+
+  aTmean <- function(TAir,nYears){
+    Tmean = colMeans(matrix(TAir,365,nYears))
+    return(Tmean)
+  }
+
+  aTampl <- function(TAir,nYears){
+    monthsDays <- c(rep(1,31),rep(2,28),rep(3,31),rep(4,30),rep(5,31),rep(6,30),
+                    rep(7,31),rep(8,31),rep(9,30),rep(10,31),rep(11,30),rep(12,31))
+    TbyYear <- matrix(TAir,365,nYears)
+    Tampl = apply(TbyYear, 2, function(x) max(aggregate(x/2,by=list(monthsDays),FUN=mean)) - min(aggregate(x/2,by=list(monthsDays),FUN=mean))  )
+    return(Tampl)
+  }
+
+  aPrecip <- function(Precip,nYears){
+    aP = colSums(matrix(Precip,365,nYears))
+    return(aP)
+  }
